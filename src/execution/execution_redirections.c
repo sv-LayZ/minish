50a1a93ca9b3baa50223b99089f8bce1e084b245
dborian <edmedorian@gmail.com>
2025-09-16 08:11:11 +0200

    repush execution_redirection.c

--- src/execution/execution_redirections.c
+++ src/execution/execution_redirections.c
@@ -59,3 +59,54 @@ static int	execute_child_with_redirections(t_cmd *cmd, char *executable_path)
 	}
 	return (0);
 }
+
+static int	fork_and_execute_external(t_cmd *cmd, char *executable_path)
+{
+	pid_t	pid;
+	int		status;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		free(executable_path);
+		return (1);
+	}
+	if (pid == 0)
+		execute_child_with_redirections(cmd, executable_path);
+	else
+	{
+		free(executable_path);
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+			return (WEXITSTATUS(status));
+		return (128 + WTERMSIG(status));
+	}
+	return (0);
+}
+
+int	execute_external_with_redirections(t_cmd *cmd)
+{
+	char	*executable_path;
+
+	executable_path = find_executable_in_path(cmd->args[0]);
+	if (!executable_path)
+	{
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd(cmd->args[0], 2);
+		ft_putstr_fd(": command not found\n", 2);
+		return (127);
+	}
+	return (fork_and_execute_external(cmd, executable_path));
+}
+
+int	execute_with_redirections(t_cmd *cmd)
+{
+	int	builtin_index;
+
+	builtin_index = is_builtin(cmd->args[0]);
+	if (builtin_index != -1)
+		return (execute_builtin_with_redirections(cmd, builtin_index));
+	else
+		return (execute_external_with_redirections(cmd));
+}
